08b62cb73b709ba95898fe51abb4cda578824bc7
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-04-14 23:52:57 +0700

    Add Navigation for change screen

--- index.android.js
+++ index.android.js
@@ -6,16 +6,63 @@
 
 import React, { Component } from 'react'
 import {
-  AppRegistry
+  AppRegistry,
+  Navigator,
+  Text
 } from 'react-native'
 
 import MainScreen from './MainScreen'
 
+const routes = [
+  {index: 0 },
+  {index: 1 }
+]
+
 export default class NeverForgotYourLocation extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showNavBar: false
+    }
+  }
+  renderScene(route, navigator) {
+    if (route.index === 0) return <MainScreen navigator={navigator} routes={routes} />
+    if (route.index === 1) return <Text>Hehe</Text>
+  }
   render() {
-    return (
-      <MainScreen />
-    )
+    if (this.state.showNavBar)
+      return (
+        <Navigator
+          initialRoute={routes[0]}
+          initialRouteStack={routes}
+          renderScene={this.renderScene}
+          configureScene={(route, routeStack) => Navigator.SceneConfigs.HorizontalSwipeJump}
+          navigationBar={
+            <Navigator.NavigationBar
+              routeMapper={{
+                LeftButton: (route, navigator, index, navState) => {
+                  return <Text>Cancel</Text>
+                },
+                RightButton: (route, navigator, index, navState) => {
+                  return <Text>Done</Text>
+                },
+                Title: (route, navigator, index, navState) => {
+                  return <Text>Awesome Nav Bar</Text>
+                },
+              }}
+            />
+          }
+        />
+      )
+    else
+      return (
+        <Navigator
+          initialRoute={routes[0]}
+          initialRouteStack={routes}
+          renderScene={this.renderScene}
+          configureScene={(route, routeStack) => Navigator.SceneConfigs.HorizontalSwipeJump}
+        />
+      )
   }
 }
 
