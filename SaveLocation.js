bd6e240f27402b3cad5cb09362cf485d6a7f70ef
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-05-01 14:46:13 +0700

    Update Save Location, load locations was saved before and show it

--- SaveLocation.js
+++ SaveLocation.js
@@ -5,110 +5,184 @@ import {
   Text,
   TextInput,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
+  ToastAndroid,
+  AsyncStorage
 } from 'react-native'
-import Mapbox, { MapView } from 'react-native-mapbox-gl'
+import Spinner from 'react-native-spinkit'
+import Mapbox, { MapView } from './src/config/Mapbox'
 
-const accessToken = 'sk.eyJ1IjoiaGllcHZ2IiwiYSI6ImNqMXN5ZmJqNzAwMjMzMnJ6ZzR2d2huZWoifQ.LSja8rK5NJny2L0pGWLpfA'
-Mapbox.setAccessToken(accessToken)
+const icons = {
+  user: require('./src/assets/imgs/user.png'),
+  place: require('./src/assets/imgs/placeholder.png')
+}
 
 export default class SaveLocation extends Component {
+  static navigationOptions = {
+    title: 'SAVE LOCATION'
+  }
   constructor(props) {
     super(props)
     this.state = {
+      isLoading: true,
       coordinate: {
-        longitude: 0,
-        latitude: 0
+        longitude: null,
+        latitude: null
       },
-      placeName: '',
-      placeAddress: ''
+      annotations: null,
+      placeName: null,
+      placeAddress: null
     }
   }
   componentDidMount() {
+    // AsyncStorage.clear((err)=>{})
     navigator.geolocation.getCurrentPosition((position) => {
       let coordinate = {}
       coordinate.longitude = position.coords.longitude
       coordinate.latitude = position.coords.latitude
-      this.setState({ coordinate })
-      this._map.setCenterCoordinate(this.state.coordinate.latitude, this.state.coordinate.longitude, animated = true)
-    },
-      (error) => console.warn(JSON.stringify(error)),
-      { enableHighAccuracy: true, timeout: 5000, maximumAge: 1000 }
-    )
+      this.setState({
+        coordinate: coordinate
+      })
+    })
+    AsyncStorage.getAllKeys((err, keys) => {
+      AsyncStorage.multiGet(keys, (err, locations) => {
+        let annotations = []
+        locations.map((result, idx, location) => {
+          let data = JSON.parse(location[idx][1])
+          let annotation = {
+            coordinates: data.coordinates,
+            type: 'point',
+            title: data.placeName,
+            subtitle: data.placeAddress,
+            id: location[idx][0]
+          }
+          annotations.push(annotation)
+        })
+        this.setState({
+          annotations: annotations,
+          isLoading: false
+        })
+      })
+    })
   }
-  onButtonPress() {
-    let data = {
-      coordinate: this.state.coordinate,
-      placename: this.state.placeName,
-      placeaddress: this.state.placeAddress
+  onButtonSavePress(navigate) {
+    if (!this.state.placeName) {
+      ToastAndroid.showWithGravity('Please type your place name!',
+        ToastAndroid.LONG, ToastAndroid.CENTER)
+    } else if (!this.state.placeAddress) {
+      ToastAndroid.showWithGravity('Please type your place address!',
+        ToastAndroid.LONG, ToastAndroid.CENTER)
+    } else if ((!this.state.coordinate.latitude) || (!this.state.coordinate.longitude)) {
+      ToastAndroid.showWithGravity('Cannot get your location, please try later!',
+        ToastAndroid.LONG, ToastAndroid.CENTER)
+    } else {
+      let uid = this.state.coordinate.latitude.toString() + this.state.coordinate.longitude.toString()
+      let dataToSave = {
+        coordinates: [this.state.coordinate.latitude, this.state.coordinate.longitude],
+        placeName: this.state.placeName,
+        placeAddress: this.state.placeAddress
+      }
+      AsyncStorage.setItem(uid, JSON.stringify(dataToSave), () => {
+        AsyncStorage.getItem(uid, (err, location) => {
+          let data = JSON.parse(location)
+          let annotations = this.state.annotations.concat()
+          let annotation = {
+            coordinates: data.coordinates,
+            type: 'point',
+            title: data.placeName,
+            subtitle: data.placeAddress,
+            id: uid
+          }
+          annotations.push(annotation)
+          this.setState({ annotations })
+        })
+      })
+      ToastAndroid.show('Save Location successfully!', ToastAndroid.LONG)
+      setTimeout(() => {
+        navigate('Home')
+      }, 500)
     }
-    console.warn(JSON.stringify(data))
   }
   render() {
-    return (
-      <View style={styles.container}>
-        <MapView style={styles.mapView}
-          ref={map => { this._map = map }}
-          initialCenterCoordinate={this.state.coordinate}
-          initialZoomLevel={15}
-          showsUserLocation={true}
-          styleURL={Mapbox.mapStyles.streets}
-
-        />
-        <View style={styles.inforLocation}>
-          <View style={styles.placeContainer}>
-            <View style={styles.placeIcon}>
-              <Image source={require('./src/imgs/user.png')}></Image>
-            </View>
-            <TextInput style={styles.placeName}
-              placeholder="Place name"
-              onChangeText={(placeName) => this.setState({ placeName })}
-              multiline={false}
-              underlineColorAndroid={'transparent'}
-              value={this.state.placeName}
-            />
-          </View>
-          <View style={styles.borderContainer}>
-            <View style={styles.borderWhite}></View>
-            <View style={styles.borderView}></View>
-          </View>
-          <View style={styles.placeContainer}>
-            <View style={styles.placeIcon}>
-              <Image source={require('./src/imgs/placeholder.png')}></Image>
-            </View>
-            <TextInput style={styles.placeAddress}
-              placeholder="Place address"
-              onChangeText={(placeAddress) => this.setState({ placeAddress })}
-              multiline={false}
-              underlineColorAndroid={'transparent'}
-              value={this.state.placeAddress}
-            />
+    const { navigate } = this.props.navigation
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.mainContainter}>
+          <View style={styles.loadingContainer}>
+            <Spinner size={100} color={'#358ff4'} type={'Bounce'} />
           </View>
         </View>
-        <View style={styles.areaSave}>
-          <TouchableOpacity style={styles.buttonSave} onPress={() => this.onButtonPress()}>
-            <Text style={styles.labelSave}>
-              SAVE
+      )
+    } else {
+      return (
+        <View style={styles.mainContainter}>
+          <MapView style={styles.mapContainer}
+            ref={map => { this._map = map }}
+            initialCenterCoordinate={this.state.coordinate}
+            initialZoomLevel={15}
+            showsUserLocation={true}
+            styleURL={Mapbox.mapStyles.streets}
+            annotations={this.state.annotations}
+          />
+          <View style={styles.saveContainer}>
+            <View style={styles.placeContainer}>
+              <View style={styles.placeIcon}>
+                <Image source={icons.user}></Image>
+              </View>
+              <TextInput style={styles.placeText}
+                placeholder="Place name"
+                onChangeText={(placeName) => this.setState({ placeName })}
+                multiline={false}
+                underlineColorAndroid={'transparent'}
+                value={this.state.placeName}
+              />
+            </View>
+            <View style={styles.borderContainer}>
+              <View style={styles.borderWhite}></View>
+              <View style={styles.borderView}></View>
+            </View>
+            <View style={styles.placeContainer}>
+              <View style={styles.placeIcon}>
+                <Image source={icons.place}></Image>
+              </View>
+              <TextInput style={styles.placeText}
+                placeholder="Place address"
+                onChangeText={(placeAddress) => this.setState({ placeAddress })}
+                multiline={false}
+                underlineColorAndroid={'transparent'}
+                value={this.state.placeAddress}
+              />
+            </View>
+          </View>
+          <View style={styles.saveContent}>
+            <TouchableOpacity style={styles.saveButton}
+              onPress={() => this.onButtonSavePress(navigate)}>
+              <Text style={styles.saveText}>
+                SAVE
              </Text>
-          </TouchableOpacity>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-
-    )
+      )
+    }
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainter: {
     flex: 1,
-    paddingTop: 60,
     alignItems: 'stretch',
     backgroundColor: '#FFFFFF'
   },
-  mapView: {
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  mapContainer: {
     flex: 1
   },
-  inforLocation: {
+  saveContainer: {
     height: 121,
     flexDirection: 'column',
     justifyContent: 'center'
@@ -117,55 +191,48 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   placeIcon: {
-    flex: 0.1,
+    flex: 0.15,
     justifyContent: 'center',
     alignItems: 'center'
   },
-  placeName: {
-    flex: 0.9,
+  placeText: {
+    flex: 0.85,
     color: '#000000',
     fontSize: 16,
     fontFamily: "Helvetica",
     fontWeight: 'normal',
-    paddingLeft: 7
+    paddingLeft: 7,
+    fontFamily: 'ProximaNovaSoft-Regular'
   },
   borderContainer: {
     flexDirection: 'row'
   },
   borderWhite: {
-    flex: 0.1,
+    flex: 0.15,
     height: 1,
     backgroundColor: '#FFFFFF'
   },
   borderView: {
-    flex: 0.9,
+    flex: 0.85,
     height: 1,
     backgroundColor: '#757575'
   },
-  placeAddress: {
-    flex: 0.9,
-    color: '#000000',
-    fontSize: 16,
-    fontFamily: "Helvetica",
-    fontWeight: 'normal',
-    paddingLeft: 7
-  },
-  areaSave: {
+  saveContent: {
     alignItems: 'center',
     justifyContent: 'center'
   },
-  buttonSave: {
+  saveButton: {
     height: 60,
     minWidth: '100%',
     backgroundColor: '#FD482E',
     justifyContent: 'center',
     alignItems: 'center'
   },
-  labelSave: {
+  saveText: {
     color: '#F1F5F6',
     textAlign: 'center',
     fontSize: 20,
-    fontFamily: "Helvetica",
+    fontFamily: "ProximaNovaSoft-Medium",
     fontWeight: 'bold'
   }
 })
