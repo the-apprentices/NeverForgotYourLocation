5f60f92a6840ace0f1b18c690937d4934f5c6e79
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-05-01 14:42:15 +0700

    Update android index file, Using new Navigation

--- index.android.js
+++ index.android.js
@@ -1,69 +1,15 @@
 import React, { Component } from 'react'
 import {
-  StyleSheet,
-  AppRegistry,
-  Navigator
+  AppRegistry
 } from 'react-native'
+import { StackNavigator } from 'react-navigation'
 
-import { LeftButton, RightButton, Title } from './NavigationBar'
-import MainScreen from './MainScreen'
+import HomeScreen from './HomeScreen'
 import SaveLocation from './SaveLocation'
 
-const routes = [
-  { index: 0 },
-  { index: 1 }
-]
-
-export default class NeverForgotYourLocation extends Component {
-  constructor(props) {
-    super(props)
-  }
-  renderScene(route, navigator) {
-    switch (route.index) {
-      case 0:
-        return <MainScreen
-          navigator={navigator}
-          routes={routes}
-        />
-        break
-      case 1:
-        return <SaveLocation
-          navigator={navigator}
-          routes={routes} />
-        break
-      case 2:
-        break
-    }
-  }
-  render() {
-    return (
-      <Navigator
-        initialRoute={routes[0]}
-        initialRouteStack={routes}
-        renderScene={this.renderScene}
-        configureScene={(route, routeStack) =>
-          Navigator.SceneConfigs.HorizontalSwipeJump}
-        navigationBar={
-          <Navigator.NavigationBar style={styles.navigationBar}
-            routeMapper={{
-              LeftButton,
-              RightButton,
-              Title
-            }}
-          />
-        }
-      />
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  navigationBar: {
-    height: 60,
-    // backgroundColor: '#FFFFFF',
-    alignItems: 'center',
-  },
+const NeverForgotYourLocation = StackNavigator({
+  Home: { screen: HomeScreen },
+  SaveLocation: { screen: SaveLocation }
 })
-
 
 AppRegistry.registerComponent('NeverForgotYourLocation', () => NeverForgotYourLocation)
