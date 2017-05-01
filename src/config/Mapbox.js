8a2a49d5ec68438ab35946f7079056ef7d8bfa5e
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-05-01 14:51:47 +0700

    Refactor config Mapbox to another file

---
+++ src/config/Mapbox.js
@@ -0,0 +1,6 @@
+import Mapbox, { MapView } from 'react-native-mapbox-gl'
+const accessToken = 'sk.eyJ1IjoiaGllcHZ2IiwiYSI6ImNqMXN5ZmJqNzAwMjMzMnJ6ZzR2d2huZWoifQ.LSja8rK5NJny2L0pGWLpfA'
+Mapbox.setAccessToken(accessToken)
+
+export {MapView}
+export default Mapbox
