9044510526ccfb9311c83c2f5a76280e7d8119ef
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-05-07 23:36:02 +0700

    Create Visited Locations screen

--- VisitedLocations.js
+++ VisitedLocations.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {
   StyleSheet,
   View,
+  ViewPagerAndroid,
   Image,
   Text,
   TouchableNativeFeedback
@@ -9,6 +10,7 @@ import {
 import { NavigationActions } from 'react-navigation'
 import { TabNavigator } from 'react-navigation'
 
+import SwitchButton from './SwitchButton'
 import ViewFriends from './ViewFriends'
 import ViewLocations from './ViewLocations'
 
@@ -22,10 +24,32 @@ const styles = StyleSheet.create({
     width: 60,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  mainContainer: {
+    flex: 1
+  },
+  viewPagerContainer: {
+    flex: 1
+  },
+  switchButtonContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 15,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+    justifyContent: 'flex-end'
   }
 })
 
 export default class VisitedLocations extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pageNumber: 0,
+      isLeft: true
+    }
+  }
   static navigationOptions = ({ navigation }) => {
     return {
       title: 'VISITED LOCATIONS',
@@ -45,9 +69,44 @@ export default class VisitedLocations extends Component {
       </TouchableNativeFeedback>
     }
   }
+  onPageSelected = (e) => {
+    this.setState({
+      pageNumber: e.nativeEvent.positon
+    })
+  }
+  onSwitchButtonPress(buttonState) {
+    this.setState({
+      isLeft: buttonState
+    })
+    this.onSwitchButtonSelected()
+  }
+  onSwitchButtonSelected() {
+    if (this.state.isLeft) {
+      this.viewPager.setPage(0)
+    } else {
+      this.viewPager.setPage(1)
+    }
+  }
   render() {
     return (
-      <View></View>
+      <View style={styles.mainContainer}>
+        <ViewPagerAndroid style={styles.viewPagerContainer}
+          ref={(viewPager) => { this.viewPager = viewPager }}
+          initialPage={0}
+          onPageSelected={this.onPageSelected}>
+          <View>
+            <ViewLocations />
+          </View>
+          <View>
+            <ViewFriends />
+          </View>
+        </ViewPagerAndroid>
+        <View style={styles.switchButtonContainer}>
+          <SwitchButton onSwitchButtonPress={this.onSwitchButtonPress.bind(this)}
+          />
+        </View>
+      </View>
     )
   }
 }
+
