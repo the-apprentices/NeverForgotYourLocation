b11359cd2e3a7c830e9dbf80e1031f91e16bfa83
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-05-09 00:18:32 +0700

    Update MapView, just show MapView and do nothing yet

--- ViewLocations.js
+++ ViewLocations.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 import {
   StyleSheet,
-  Text
+  Text,
+  View
 } from 'react-native'
+import Mapbox, { MapView } from './src/config/Mapbox'
 
 export default class ViewLocations extends Component {
   render() {
-    return <Text>View Locations</Text>
+    return (
+      <View style={styles.mainContainer}>
+        <MapView style={styles.mapContainer}
+          initialZoomLevel={15}
+          showsUserLocation={true}
+          styleURL={Mapbox.mapStyles.streets}>
+        </MapView>
+      </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#ffffff'
+  },
+  mapContainer: {
+    flex: 1
+  }
+})
