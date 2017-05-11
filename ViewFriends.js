e30b3e00c9e5e1ddb5204085d8a907fb5983b06b
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-05-11 17:07:24 +0700

    Show Friend Locations list were save before

--- ViewFriends.js
+++ ViewFriends.js
@@ -1,61 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import {
   StyleSheet,
   View,
   ListView,
   Text,
-  Image,
-  AsyncStorage
+  Image
 } from 'react-native'
 
 export default class ViewFriends extends Component {
   constructor(props) {
     super(props)
     this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 != r2 })
-    this.state = {
-      listFriendLocations: [],
-      dataSource: this.ds.cloneWithRows([]),
-      isLoading: true
-    }
   }
-  componentDidMount() {
-    this.getListFriends()
-      .then((listFriendLocations) => {
-        this.setState({
-          listFriendLocations: listFriendLocations,
-          dataSource: this.ds.cloneWithRows(listFriendLocations)
-        })
-      })
-  }
-  getListFriends = async () => {
-    let keys = await this.getListKeys()
-    let locations = await this.getListLocations(keys)
-    let listFriendLocations = []
-    locations.map((result, idx, location) => {
-      let data = JSON.parse(location[idx][1])
-      let friendLocation = {
-        title: data.placeName,
-        subtitle: data.placeAddress,
-        id: location[idx][0]
-      }
-      listFriendLocations.push(friendLocation)
-    })
-    return listFriendLocations
-  }
-  getListKeys = async () => {
-    let keys = await AsyncStorage.getAllKeys((err, keys) => keys)
-    return keys
-  }
-  getListLocations = async (keys) => {
-    let locations = await AsyncStorage.multiGet(keys, (err, locations) => locations)
-    return locations
+  static propTypes = {
+    listFriendData: PropTypes.array.isRequired
   }
   render() {
     return (
       <View style={styles.mainContainer}>
         <ListView style={styles.listFriends}
           enableEmptySections={true}
-          dataSource={this.state.dataSource}
+          dataSource={this.ds.cloneWithRows(this.props.listFriendData)}
           renderRow={(data) => <Friend {...data} />}
         />
       </View>
@@ -84,7 +49,6 @@ class Friend extends Component {
           </View>
         </View>
         <View style={styles.dateContainer}>
-
         </View>
       </View>
     )
