e2bf527ad4239d2c28060984369c6f76911f93a8
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-06-14 00:52:02 +0700

    Remove mapbox and add google map service

--- android/app/src/main/java/com/neverforgetyourlocation/MainApplication.java
+++ android/app/src/main/java/com/neverforgetyourlocation/MainApplication.java
@@ -3,8 +3,7 @@ package com.neverforgetyourlocation;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSpinkitPackage(),
-            new ReactNativeMapboxGLPackage()
+            new MapsPackage()
       );
     }
   };
