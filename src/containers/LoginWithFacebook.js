d662854348c543ebfa89efd7a213058cb8e38fe3
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-06-26 13:24:34 +0700

    Control login facebook

--- src/containers/LoginWithFacebook.js
+++ src/containers/LoginWithFacebook.js
@@ -1,63 +1,26 @@
 import React, { Component, PropTypes } from 'react'
-import FBSDK from 'react-native-fbsdk'
 import FacebookLoginButton from '../components/FacebookLoginButton'
-
-const {
-  LoginManager,
-  AccessToken,
-  GraphRequest,
-  GraphRequestManager,
-} = FBSDK
-const infoRequest = new GraphRequest(
-  '/me',
-  null,
-  this._responseInfoCallback,
-);
-_responseInfoCallback = function (error, result) {
-  if (error) {
-    alert('Error fetching data: ' + error.toString());
-  } else {
-    console.warn('Success fetching data:' + JSON.stringify(result));
-  }
-}
 
 export default class LoginWithFacebook extends Component {
   constructor(props) {
     super(props)
   }
   static propTypes = {
-    buttonWrapperStyle: PropTypes.number.isRequired
+    buttonWrapperStyle: PropTypes.number.isRequired,
+    buttonText: PropTypes.string.isRequired,
+    onButtonPress: PropTypes.func.isRequired
   }
 
-
-  onLoginButtonPress() {
-    LoginManager.logInWithReadPermissions(['public_profile']).then(
-      function (result) {
-        if (result.isCancelled) {
-          alert('Login cancelled');
-        } else {
-          AccessToken.getCurrentAccessToken().then(
-            (data) => {
-              console.warn(JSON.stringify(data.accessToken.toString()))
-            }
-          )
-          console.warn(JSON.stringify(result))
-          console.warn('Login success with permissions: '
-            + result.grantedPermissions.toString());
-        }
-        new GraphRequestManager().addRequest(infoRequest).start();
-      },
-      function (error) {
-        alert('Login fail with error: ' + error);
-      }
-    );
+  onButtonPress() {
+    this.props.onButtonPress()
   }
 
   render() {
     return (
       <FacebookLoginButton
         buttonWrapperStyle={this.props.buttonWrapperStyle}
-        onButtonPress={this.onLoginButtonPress} />
+        buttonText={this.props.buttonText}
+        onButtonPress={() => this.onButtonPress()} />
     )
   }
 }
