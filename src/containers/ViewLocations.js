5f80f1502f479b1adb2172ba85da045088843fb7
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-06-26 13:19:14 +0700

    Refactor View Location

---
+++ src/containers/ViewLocations.js
@@ -0,0 +1,51 @@
+import React, { Component, PropTypes } from 'react'
+import {
+  StyleSheet,
+  View,
+} from 'react-native'
+import MapView from './MapView'
+
+export default class ViewLocations extends Component {
+  constructor(props) {
+    super(props)
+  }
+  static propTypes = {
+    mapRef: PropTypes.func.isRequired,
+    region: PropTypes.shape({
+      latitude: PropTypes.number.isRequired,
+      longitude: PropTypes.number.isRequired,
+      latitudeDelta: PropTypes.number.isRequired,
+      longitudeDelta: PropTypes.number.isRequired,
+    }),
+    listMarkers: PropTypes.arrayOf(PropTypes.shape({
+      coordinate: PropTypes.shape({
+        latitude: PropTypes.number.isRequired,
+        longitude: PropTypes.number.isRequired
+      }).isRequired,
+      description: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired
+    }).isRequired).isRequired,
+    keySelected: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <MapView mapRef={this.props.mapRef}
+          region={this.props.region}
+          listMarkers={this.props.listMarkers}
+          keySelected={this.props.keySelected}
+          isSavingState={false}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: 'stretch',
+    backgroundColor: '#FFFFFF'
+  }
+})
