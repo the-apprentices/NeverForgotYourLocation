d48e4a986c39f96fe90d8f6ccd10e3dbe77d7f50
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-06-26 13:20:09 +0700

    Remove conig Mapbox

--- src/config/Mapbox.js
+++
@@ -1,6 +0,0 @@
-import Mapbox, { MapView } from 'react-native-mapbox-gl'
-const accessToken = 'sk.eyJ1IjoiaGllcHZ2IiwiYSI6ImNqMXN5ZmJqNzAwMjMzMnJ6ZzR2d2huZWoifQ.LSja8rK5NJny2L0pGWLpfA'
-Mapbox.setAccessToken(accessToken)
-
-export {MapView}
-export default Mapbox
