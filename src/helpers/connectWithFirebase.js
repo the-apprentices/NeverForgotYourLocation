434e070aa6dcda9f5ab0e69901d39de10777ec95
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-07-10 14:03:49 +0700

    Update interface to call edit and delete function

--- src/helpers/connectWithFirebase.js
+++ src/helpers/connectWithFirebase.js
@@ -12,7 +12,6 @@ const authWithFirebase = (accessToken) => {
   const credential = firebase.auth.FacebookAuthProvider.credential(accessToken)
   return firebase.auth().signInWithCredential(credential)
 }
-
 const getCurrentUser = () => {
   return new Promise((resolve, reject) => {
     firebase.auth().onAuthStateChanged((user) => {
@@ -20,35 +19,54 @@ const getCurrentUser = () => {
     })
   })
 }
-
 const signOut = () => {
   return firebase.auth().signOut()
     .catch(function (error) {
       throw Error()
     })
 }
-
 const writeMarkerData = (userId, data) => {
   const newMarker = database.ref(userId + '/markers').push()
   newMarker.set(data)
 }
-
 const readAllMarkerData = (userId) => {
   return new Promise((resolve, reject) => {
     database.ref(userId + '/markers').on('value', (snapshot) => {
       let listMarkers = []
       snapshot.forEach((marker) => {
-        listMarkers.push({
-          key: marker.key,
-          coordinate: marker.val().coordinate,
-          title: marker.val().title,
-          description: marker.val().description,
-          createAt: marker.val().createAt
-        })
+        if (marker.val().visible)
+          listMarkers = [
+            {
+              key: marker.key,
+              coordinate: marker.val().coordinate,
+              title: marker.val().title,
+              description: marker.val().description,
+              createAt: marker.val().createAt,
+              visible: marker.val().visible
+            },
+            ...listMarkers
+          ]
       })
       resolve(listMarkers)
     })
   })
 }
+const updateMarkerDataInvisible = (userId, markerKey) => {
+  return database.ref(userId + '/markers/' + markerKey).update({ visible: false })
+}
+const updateMarkerDataInfor = (userId, markerKey, title, description) => {
+  return database.ref(userId + '/markers/' + markerKey).update({
+    title: title,
+    description: description
+  })
+}
 
-export { authWithFirebase, getCurrentUser, signOut, writeMarkerData, readAllMarkerData }
+export {
+  authWithFirebase,
+  getCurrentUser,
+  signOut,
+  writeMarkerData,
+  readAllMarkerData,
+  updateMarkerDataInvisible,
+  updateMarkerDataInfor
+}
