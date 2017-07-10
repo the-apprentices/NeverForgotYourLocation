43324fa8a0489cb8e8f13d46df1257f169dce9ee
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-07-10 14:24:23 +0700

    Add EditLocation into navigation

--- index.android.js
+++ index.android.js
@@ -7,11 +7,13 @@ import { StackNavigator } from 'react-navigation'
 import HomeScreen from './src/screens/HomeScreen'
 import SaveLocation from './src/screens/SaveLocation'
 import VisitedLocations from './src/screens/VisitedLocations'
+import EditLocation from './src/screens/EditLocation'
 
 const NeverForgetYourLocation = StackNavigator({
   Home: { screen: HomeScreen },
   SaveLocation: { screen: SaveLocation },
-  VisitedLocations: { screen: VisitedLocations }
+  VisitedLocations: { screen: VisitedLocations },
+  EditLocation: { screen: EditLocation}
 })
 
 AppRegistry.registerComponent('NeverForgetYourLocation', () => NeverForgetYourLocation)
