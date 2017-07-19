a7911d8ca6fbc43ce9f06232dc4000d8749a3e44
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-07-19 16:50:01 +0700

    Add actions handle locations

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,17 @@
 import { loadingAuth, signIn, signOut } from './auth'
+import { receiveLocations, addLocation } from './locations'
 import * as handleAuthentication from '../models/auth'
+import * as handleLocations from '../models/locations'
 
-export const loadingState = () => dispatch => {
+export const loadingAuthState = () => dispatch => {
   dispatch(loadingAuth())
 }
 export const getCurrentAuth = () => dispatch => {
   handleAuthentication.getCurrentAuth(auth => {
-    if (auth)
+    if (auth) {
       dispatch(signIn(auth))
+      dispatch(getLocations(auth.uid))
+    }
     else
       dispatch(signOut())
 
@@ -19,5 +23,15 @@ export const signInWithFacebook = () => dispatch => {
 export const signOutWithFacebook = () => dispatch => {
   handleAuthentication.signOutWithFacebook(() => {
     dispatch(signOut())
+  })
+}
+export const getLocations = (userId) => dispatch => {
+  handleLocations.getLocations(userId, (locations) => {
+    dispatch(receiveLocations(locations))
+  })
+}
+export const storeLocation = (uid, coordinate, title, description) => dispatch => {
+  handleLocations.saveLocation({uid, coordinate, title, description}, (location) => {
+    dispatch(addLocation(location))
   })
 }
