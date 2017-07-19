35c685fd7ef46ba8013b14c198ec86b51aaa932c
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-07-19 16:51:10 +0700

    Refactor BackButton

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,11 +1,7 @@
-import React, { Component, PropTypes } from 'react'
-import {
-  StyleSheet,
-  View,
-  Image,
-  TouchableNativeFeedback
-} from 'react-native'
-
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet, View, Image, TouchableNativeFeedback, Keyboard } from 'react-native'
+import { getAddress } from '../helpers/getData'
 const icons = {
   back: require('../assets/imgs/back-left.png')
 }
@@ -22,26 +18,35 @@ const styles = StyleSheet.create({
   }
 })
 
-export default class BackButton extends Component {
-  constructor(props) {
-    super(props)
+const onButtonPress = async (state, setParams, goBack) => {
+  if (state.params.isSavingMode) {
+    setParams({
+      isSavingMode: false,
+      displayWrapperInfor: 'none',
+      displaySaveButton: 'flex'
+    })
+    Keyboard.dismiss()
+    let placeAddress = await getAddress(state.params.targetCoordinate)
+    setParams({
+      placeName: '',
+      placeAddress: placeAddress
+    })
   }
-  static propTypes = {
-    onButtonPress: PropTypes.func.isRequired
-  }
+  else goBack()
+}
 
-  onButtonPress() {
-    this.props.onButtonPress()
-  }
-  render() {
-    return (
-      <TouchableNativeFeedback
-        onPress={() => this.onButtonPress()}
-        background={TouchableNativeFeedback.Ripple('#adadad', true)}>
-        <View style={styles.backButton}>
-          <Image style={styles.backIcon} source={icons.back} />
-        </View>
-      </TouchableNativeFeedback>
-    )
-  }
+export default BackButton = ({ state, setParams, goBack }) => (
+  <TouchableNativeFeedback
+    onPress={() => onButtonPress(state, setParams, goBack)}
+    background={TouchableNativeFeedback.Ripple('#adadad', true)}>
+    <View style={styles.backButton}>
+      <Image style={styles.backIcon} source={icons.back} />
+    </View>
+  </TouchableNativeFeedback>
+)
+
+BackButton.propTypes = {
+  state: PropTypes.object.isRequired,
+  setParams: PropTypes.func.isRequired,
+  goBack: PropTypes.func.isRequired
 }
