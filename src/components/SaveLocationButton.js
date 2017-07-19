2ce18384acf86e7fa80888174e765d6aa29e8e92
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-07-19 17:00:02 +0700

    Refactor Save Button

--- src/components/SaveLocationButton.js
+++ src/components/SaveLocationButton.js
@@ -1,43 +1,43 @@
+
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StyleSheet, View, Text, TouchableNativeFeedback } from 'react-native'
 const styles = StyleSheet.create({
   buttonWrapperStyle: {
-    ...StyleSheet.absoluteFillObject,
-    bottom: 10,
-    alignItems: 'center',
-    justifyContent: 'flex-end'
+    alignItems: 'center'
   },
   buttonContainerStyle: {
-    height: 60,
-    width: '90%',
-    backgroundColor: '#FD482E',
+    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    height: 60,
+    width: '80%',
+    backgroundColor: '#FD482E'
   },
   buttonTextStyle: {
     color: '#F1F5F6',
     textAlign: 'center',
     fontSize: 20,
-    fontFamily: 'ProximaNovaSoft-Medium',
+    fontFamily: 'ProximaNovaSoft-Regular',
     fontWeight: 'normal'
   }
 })
 
-export default SaveButton = ({ style, onButtonPress }) => (
+const SaveButton = ({ style, navigation }) => (
   <View style={[styles.buttonWrapperStyle, style]}>
     <TouchableNativeFeedback
-      onPress={() => onButtonPress()}
+      onPress={() => navigation.dispatch({ type: 'SaveLocation' })}
       background={TouchableNativeFeedback.Ripple('#adadad', false)}>
       <View style={styles.buttonContainerStyle}>
         <Text style={styles.buttonTextStyle}>
-          SAVE
+          SAVE LOCATION
             </Text>
       </View>
     </TouchableNativeFeedback>
   </View>
 )
 SaveButton.propTypes = {
-  style: PropTypes.object.isRequired,
-  onButtonPress: PropTypes.func.isRequired
+  style: PropTypes.number.isRequired,
+  navigation: PropTypes.object.isRequired
 }
+export default SaveButton
