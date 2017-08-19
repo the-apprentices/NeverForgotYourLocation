197373ddfe29b17daad178be5b9cd3a9d84f8186
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-08-19 23:12:44 +0700

    Change new marker icon's display

--- src/components/NewMarker.js
+++ src/components/NewMarker.js
@@ -1,31 +1,29 @@
-import React, { Component, PropTypes } from 'react'
-import MapView from 'react-native-maps'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet, View, Image } from 'react-native'
+const styles = StyleSheet.create({
+  markerContainer: {
+    position: 'absolute',
+    ...StyleSheet.absoluteFillObject,
+    top: '50%',
+    left: '50%',
+    marginTop: -48,
+    marginLeft: -24
+  },
+  markerSize: {
+    width: 48,
+    height: 48
+  }
+})
 const icons = {
   marker: require('../assets/imgs/marker-point.png')
 }
 
-export default class NewMarker extends Component {
-  constructor(props) {
-    super(props)
-  }
-  static propTypes = {
-    targetCoordinate: PropTypes.shape({
-      latitude: PropTypes.number.isRequired,
-      longitude: PropTypes.number.isRequired
-    }).isRequired,
-    onDragMarker: PropTypes.func.isRequired
-  }
-  onDragMarker(e) {
-    this.props.onDragMarker(e.nativeEvent.coordinate)
-  }
-  render() {
-    return (
-      <MapView.Marker
-        coordinate={this.props.targetCoordinate}
-        onDragEnd={(e) => this.onDragMarker(e)}
-         //image={icons.marker} 
-        draggable
-      />
-    )
-  }
+export default NewMarker = ({ display }) => (
+  <View style={[styles.markerContainer, { display: display }]}>
+    <Image style={styles.markerSize} source={icons.marker} />
+  </View>
+)
+NewMarker.propTypes = {
+  display: PropTypes.string.isRequired
 }
