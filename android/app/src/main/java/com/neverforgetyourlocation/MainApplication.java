3eed62e91a981fdac1bf6eeb8c6dc8c7eef6eb81
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-08-21 00:57:31 +0700

    Config new package for android

--- android/app/src/main/java/com/neverforgetyourlocation/MainApplication.java
+++ android/app/src/main/java/com/neverforgetyourlocation/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.FacebookSdk;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNCardViewPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCardViewPackage(),
             new FBSDKPackage(mCallbackManager),
             new MapsPackage()
       );
