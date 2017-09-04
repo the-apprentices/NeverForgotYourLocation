7fa105c7ee37ded5a1bbb5c83006c588a631aeea
Hiep Van Viet <viethiep.hcmut@gmail.com>
2017-09-04 22:11:49 +0700

    Update layout and add redirect to edit infor

--- src/components/LocationTextInput.js
+++ src/components/LocationTextInput.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StyleSheet, View, Image, Text } from 'react-native'
+import { StyleSheet, View, TouchableNativeFeedback, Image, Text } from 'react-native'
 const styles = StyleSheet.create({
   placeContainer: {
     flexDirection: 'row',
@@ -8,18 +8,19 @@ const styles = StyleSheet.create({
     paddingBottom: 8
   },
   placeWrap: {
-    flex: 0.15,
+    flex: 0.1,
     justifyContent: 'center',
     alignItems: 'center'
   },
   placeIcon: {
-    width: 30,
-    height: 30
+    width: 24,
+    height: 24
   },
   placeText: {
-    flex: 0.85,
-    marginLeft: 8,
-    marginRight: 12
+    flex: 0.9,
+    marginLeft: 4,
+    marginRight: 8,
+    justifyContent: 'center'
   },
   placeContent: {
     color: '#000000',
@@ -28,21 +29,37 @@ const styles = StyleSheet.create({
   }
 })
 
-export default LocationTextInput = ({ icon, placeHolder, textValue, onChangeText }) => (
-  <View style={styles.placeContainer}>
-    <View style={styles.placeWrap}>
-      <Image style={styles.placeIcon} source={icon}></Image>
+export default LocationTextInput = ({ icon, placeHolder, textValue, suggestionList, onChangeText, navigation }) => (
+  <TouchableNativeFeedback
+    onPress={() => {
+      navigation.dispatch({
+        type: 'SuggestionScreen',
+        params: {
+          placeHolder: placeHolder,
+          textValue: textValue,
+          suggestionList: suggestionList,
+          onChangeText: onChangeText
+        }
+      })
+    }}
+    background={TouchableNativeFeedback.Ripple('#adadad', false)}>
+    <View style={styles.placeContainer}>
+      <View style={styles.placeWrap}>
+        <Image style={styles.placeIcon} source={icon}></Image>
+      </View>
+      <View style={styles.placeText}>
+        <Text style={styles.placeContent}>
+          {textValue}
+        </Text>
+      </View>
     </View>
-    <View style={styles.placeText}>
-      <Text style={styles.placeContent}
-        onPress={() => { }}>{textValue}
-      </Text>
-    </View>
-  </View>
+  </TouchableNativeFeedback>
 )
 LocationTextInput.propTypes = {
   icon: PropTypes.number.isRequired,
   placeHolder: PropTypes.string.isRequired,
   textValue: PropTypes.string.isRequired,
-  onChangeText: PropTypes.func.isRequired
+  suggestionList: PropTypes.array.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+  navigation: PropTypes.object.isRequired
 }
